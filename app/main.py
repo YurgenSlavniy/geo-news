b21405881b97e00eef6a2792292aa8f675dd842e
aiva <andrey.cherkessk@yandex.ru>
2023-02-15 20:57:10 +0300

    added full-text search and divided the window into two parts

--- app/main.py
+++ app/main.py
@@ -26,10 +26,10 @@ CHANELS = [
     "https://www.interfax.ru/rss.asp",
 ]
 # словарь с ключами совпадающими с id страны на карте с
-# списками свзанных слов
+# списками свзанных слов через ||
 COUTRIES = {
-    "RU": ["РФ", "Российска Федерация", "Россия"],
-    "US": ["США", "Соединённые Штаты Америки", "Америка"],
+    "RU": "РФ OR Российская OR Федерация OR Россия OR России",
+    "US": "США OR Соединённые OR Штаты OR Америки OR Америка",
 }
 
 # текущеее имя БД
@@ -38,12 +38,8 @@ dbname = ""
 dbname_parse = ""
 # список старых ибен БД для удаления 
 dbname_old_list = []
-
-QUERY_CREATE_TABLE = """CREATE TABLE IF NOT EXISTS news_content(
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    link TEXT,
-    media TEXT);"""
+# IF NOT EXISTS 
+QUERY_CREATE_TABLE = """CREATE VIRTUAL TABLE news_content USING FTS5(title,link,media);"""
 
 def parser():
     '''собирает заголовки новостей и помещает их базу данных'''
@@ -59,6 +55,8 @@ def parser():
 
     for i, chanel in enumerate(CHANELS):
         try:
+            # делаем запрос для получения данных
+            # если ошибка переходим к следующей итерации
             response = requests.get(chanel)
         except Exception as e:
             continue
@@ -66,6 +64,7 @@ def parser():
             soup = bs(response.text, 'xml')
             for item in soup.find_all("item"):
                 try:
+                    # если нет url будет ошибка, пропускаем её
                     media = item.find("enclosure").get("url")
                 except Exception as e:
                     media = ""
@@ -74,9 +73,11 @@ def parser():
                 query = "INSERT INTO news_content(title, link, media) VALUES(?, ?, ?)"
                 cursor.execute(query, (title, link, media))
         connection.commit()
-
+    # если было имя БД в глобальной переменной
+    # добовляем к списку файлов для удаления
     if os.path.isfile(dbname_parse):
         dbname_old_list.append(dbname_parse)
+    # dbname_parse используется в action -> parse_news
     dbname_parse = dbname
 
     print("dbname_old_list:", dbname_old_list)
@@ -84,6 +85,7 @@ def parser():
     connection.close()
 
 def clear_news():
+    '''удаление фалов БД'''
     while True:
         if len(dbname_old_list) == 0:
             break
@@ -93,9 +95,11 @@ def clear_news():
             os.remove(file_name)
 
 def close_app():
+    '''выполняется при завершении сервера'''
     dbname_old_list.append(dbname)
     clear_news()
 
+# регистрация функции завершения
 atexit.register(close_app)
 
 class Config:
@@ -107,14 +111,15 @@ app.config.from_object(Config())
 scheduler = APScheduler()
 scheduler.init_app(app)
 
-# ПЕРВЫЕ НОВОСТИ ПОСЛЕ ЗАПУСКА ПРИЛОЖЕНИЯ БУДУТ ЧЕРЕЗ 60 секунд
 @scheduler.task('interval', id='parse_new_news', seconds=60, max_instances=1)
 def parse_new_news():
+    '''первые заголовки новостей будут после запуска приложения будут через 60 секунд'''
     print("[RUN PARSER]")
     parser()
 
 @scheduler.task('interval', id='delete_old_news', seconds=25, max_instances=1)
 def delete_old_news():
+    '''удаление файлов БД во время работы'''
     clear_news()
 
 scheduler.start()
@@ -126,21 +131,26 @@ def index():
         "title": "Geo News!",
     })
 
-@app.route("/parse/news/<countryid>/")
-def parse_news(countryid):
+@app.route("/parse/news/<leftID>/<rightID>/")
+def parse_news(leftID, rightID):
     '''здесь нужно отдать новости fronend-у'''
     global dbname_parse
-    # words список слов для отбора заголовков для конкретной страны
-    related_words = COUTRIES.get(countryid, "RU")
-    pprint(related_words)
+    print("countries code:", leftID, rightID)
+    # выбраные области на карте их ID
+    related_words_left = COUTRIES.get(leftID, "РФ OR Российская OR Федерация OR Россия OR России")
+    related_words_right = COUTRIES.get(rightID, "США OR Соединённые OR Штаты OR Америки OR Америка")
+
+    pprint([related_words_left, related_words_right])
     print("dbname_parse:", dbname_parse)
+
     if os.path.isfile(dbname_parse):
         connection = sqlite3.connect(dbname_parse)
         cursor = connection.cursor()
-        result = cursor.execute("SELECT title, link, media FROM news_content").fetchall()
-        # result список заголовкм новостей
-        if result is not None:
+        # полно-текстой поиск по БД по словам через или
+        l = cursor.execute(f"SELECT title, link, media FROM news_content WHERE title MATCH '{related_words_left}'").fetchall()
+        r = cursor.execute(f"SELECT title, link, media FROM news_content WHERE title MATCH '{related_words_right}'").fetchall()
+        if l and r:
             connection.close()
-            return json.dumps(result)
+            return json.dumps({"left": l, "right": r})
 
-    return json.dumps([])
+    return json.dumps({"left": [], "right": []})
